b4655c086f7c52f113855fbfe212739a2a0e3323
Cenxuan Tian <cenxuan.tian@outlook.com>
2024-07-07 14:59:33 +0200

    feat(example):add some examples

---
+++ examples/load_dll_test.cpp
@@ -0,0 +1,14 @@
+#include <windows.h>
+#include <iostream>
+#include "D:\User\Desktop\git_proj\t_pic_processor\t_pic_processor.h"
+
+
+int main(){
+
+    pic_process_rgba(0,0,0,0);
+
+
+    std::cout << "hello\n";
+
+    return 0;
+}
