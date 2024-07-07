1f634af3010f507e34d95236e58bbea4e87c9bfd
Cenxuan Tian <cenxuan.tian@outlook.com>
2024-07-07 15:40:35 +0200

    fix(example):fixed some bugs in the examples

--- examples/load_dll_test.cpp
+++ examples/load_dll_test.cpp
@@ -1,14 +1,23 @@
 #include <windows.h>
 #include <iostream>
-#include "D:\User\Desktop\git_proj\t_pic_processor\t_pic_processor.h"
 
+using pic_process_rgba_t = int (*)(void* buf, int width, int height, int pixel);
 
 int main(){
 
-    pic_process_rgba(0,0,0,0);
+    HMODULE h = LoadLibraryA( "D:/User/Desktop/git_proj/nvcc_test/x64/Release/t_pic_processor.dll");
+    if(!h) return -1;
+    pic_process_rgba_t pic_process_rgba = (pic_process_rgba_t)GetProcAddress(h,"pic_process_rgba");
+    if(!pic_process_rgba) return -1;
+    
+    char* buf = new char[1024*4];
+    memset(buf,1,1024*4);
+    int ret = pic_process_rgba(buf,1,1,1024);
+    
+	for (int i = 0; i < 1024*4; i++) {
+		printf("%d ", ((char*)(buf))[i]);
+	}
 
-
-    std::cout << "hello\n";
-
+    delete[] buf;
     return 0;
 }
