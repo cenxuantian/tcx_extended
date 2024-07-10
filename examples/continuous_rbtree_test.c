3b31bbe9cfebf84d875aa3572650fec1ae12b685
Cenxuan Tian <cenxuan.tian@outlook.com>
2024-07-10 22:06:30 +0200

    fix(continuous): fix some bug in continuous_rbtree

--- examples/continuous_rbtree_test.c
+++ examples/continuous_rbtree_test.c
@@ -2,6 +2,9 @@
 
 
 int main(int argc, char** argv) {
+	void* map = t_cont_rbtree_create(sizeof(int), t_cont_rbtree_mem_smaller, 10);
 
+
+	free(map);
 	return 0;
 }
--- tcx_continuous_container/t_continuous_rbtree.h
+++ tcx_continuous_container/t_continuous_rbtree.h
@@ -27,8 +27,14 @@ typedef int(*t_cont_rbtree_cmp_fn_t)(void*, void*);
 
 T_CONTINUOUS_API void* t_cont_rbtree_create(int each_size, int cmp_type, int reserve);
 T_CONTINUOUS_API void t_cont_rbtree_set_cmp_fn(void* rbtree, t_cont_rbtree_cmp_fn_t fn);
+
 T_CONTINUOUS_API void* t_cont_rbtree_emplace(void* rbtree, void* buf);
-T_CONTINUOUS_API void t_cont_rbtree_erase(void* rbtree, void* buf);
+
+T_CONTINUOUS_API int t_cont_rbtree_seaarch(void* rbtree, void* data);
+T_CONTINUOUS_API void* t_cont_rbtree_at(void* rbtree, int pos);
+T_CONTINUOUS_API void t_cont_rbtree_erase(void* rbtree, int pos);
+
+
 T_CONTINUOUS_API void* t_cont_rbtree_reserve(void* rbtree, int size);
 T_CONTINUOUS_API void* t_cont_rbtree_data(const void* rbtree);
 T_CONTINUOUS_API int t_cont_rbtree_item_size(const void* rbtree);
